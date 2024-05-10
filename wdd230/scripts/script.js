613538896835fe5bd22c54ec28a4da84267b5592
Aaron Heiner <aheiner2001@gmail.com>
2024-05-09 22:49:20 -0600

    added map

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -1,3 +1,11 @@
 document.getElementById("menu").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("open");
+    var nav = document.querySelector("nav");
+    var map = document.getElementById("map");
+    nav.classList.toggle("open");
+    this.classList.toggle("open");
+    if (nav.classList.contains("open")) {
+        map.style.display = "none";
+    } else {
+        map.style.display = "block";
+    }
 });
