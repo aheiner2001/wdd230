ed8061a8cdf2cf6b4e05b2fe5f60caab3232f10b
Aaron Heiner <aheiner2001@gmail.com>
2024-06-21 19:17:07 -0600

    changes

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,10 @@
-const baseUrl = 
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const baseUrl = "https://aheiner2001.github.io/wdd230/";
+const linksURL = "https://aheiner2001.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  console.log(data);
+}
+
+getLinks();// Call the function to execute it
