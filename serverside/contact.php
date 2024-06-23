30399b8ed576b1d20d1405fbc8ef3da28f0f677c
Aaron Heiner <aheiner2001@gmail.com>
2024-06-23 12:37:38 -0600

    phptets

--- serverside/contact.php
+++ serverside/contact.php
@@ -10,23 +10,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    $recipient = "aheiner2001@gmail.com"; // Replace with your email address
-    $subject = "New contact from $name";
-
+    // Prepare the email content
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "Message:\n$message\n";
 
-    $email_headers = "From: $name <$email>";
+    // Define the file path
+    $file_path = '/Users/aaronheiner/Documents/BYUI SPRING 2024/serverside/uploads/mail.out';
 
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
+    // Attempt to write to the file
+    if (file_put_contents($file_path, $email_content, FILE_APPEND | LOCK_EX)) {
         // Set a 200 (okay) response code.
         http_response_code(200);
-        echo "Thank You! Your message has been sent.";
+        echo "Thank You! Your message has been saved.";
     } else {
         // Set a 500 (internal server error) response code.
         http_response_code(500);
-        echo "Oops! Something went wrong, and we couldn't send your message.";
+        echo "Oops! Something went wrong, and we couldn't save your message.";
     }
 } else {
     // Not a POST request, set a 403 (forbidden) response code.
